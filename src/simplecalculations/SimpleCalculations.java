2616ccb857e2aacc80efc399c70cb0eb06a002a3
Peter Stanger <psta191@mywhitecliffe.com>
2020-05-18 19:16:12 +1200

    Completed up to Step 7

--- src/simplecalculations/SimpleCalculations.java
+++ src/simplecalculations/SimpleCalculations.java
@@ -3,7 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package simplecalculations;
+
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +18,36 @@ public class SimpleCalculations {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+              
+        String input;
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Please enter the first floating number");
+        input = sc.next();
+        float num1 = Float.valueOf(input);
+        
+        System.out.println("Please enter the second floating number");
+        input = sc.next();
+        float num2 = Float.valueOf(input);
+        
+        System.out.println("Please enter the operand");
+        input = sc.next();
+        char operand = input.charAt(0);
+
+        System.out.println(calculate(num1,num2,operand));
     }
     
+
+    private static String calculate(float num1, float num2, char operand){
+        if(operand == '+'){
+            return Float.toString(num1 + num2);
+        }
+        else if(operand == '-'){
+            return Float.toString(num1 - num2);
+        }
+        else{
+            return "Wrong operand provided";
+        }
+    }
+
 }
